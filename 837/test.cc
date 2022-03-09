423c5059a1b8c781ccdddfa5c405d426c90c83c5
LIParadise <a58524andy@gmail.com>
2022-03-09 14:42:59 +0800

    TLE
    Added data which caused TLE.

--- 837/test.cc
+++ 837/test.cc
@@ -6,6 +6,7 @@ int main() {
     std::cout << s.new21Game(10, 1, 10) << std::endl;
     std::cout << s.new21Game(6, 1, 10) << std::endl;
     std::cout << s.new21Game(21, 17, 10) << std::endl;
+    std::cout << s.new21Game(9632, 6575, 5602) << std::endl;
 
     return 0;
 }
