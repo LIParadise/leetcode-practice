492037970446e2bc8e7f2856521a6c672a0eff80
LIParadise <a58524andy@gmail.com>
2022-03-16 17:24:47 +0800

    TLE

--- 740/soln.cc
+++ 740/soln.cc
@@ -13,32 +13,95 @@
  * operation some number of times.
  */
 int Solution::deleteAndEarn(std::vector<int> &nums) {
-    // If some number n is isolated, in that if there's neither (n+1)
-    // nor (n-1) in "nums", n must be collected.
-    // Otherwise, supp. we have consecutive numbers {[n, m]}
-    // Then we inspect if the even numbers have greater sum or the odd,
-    // and collect the higher one.
-    // In fact, that the sum is from even numbers or odd numbers doesn't
-    // matter. It's the alternating sum that matters.
+    /* Dynamic programming
+     *
+     * Suppose we have {k} distinct numbers, {n_1, n_2, ..., n_k},
+     * we can store a {k \times k} table (let the table be T)
+     * s.t. T[i][j] marks the answer to the {n_i, n_{i+1}, ..., n_{j}}
+     * For example, suppose "nums" is the following
+     * [1, 1, 3, 4, 5, 5, 5, 5, 5, 5, 6, 6, 6]
+     * Then T[2][3] stores the answer to the following:
+     *       [3, 4, 5, 5, 5, 5, 5, 5]
+     * Notice first that in "T", it stores the order statistics, not the
+     * actual number in "nums"
+     * Notice further that {i \leq j} always holds.
+     * (We only need to store half of a square matrix)
+     *
+     * Suppose we know T[i][j] and T[j+1][k], what's T[i][k]?
+     * If the j-th smallest and the (j+1)-th smallest differ by
+     * larger or equal to 2, then T[i][k] = T[i][j] + T[j+1][k]
+     * (The ordering is done ignoring all equal terms)
+     *
+     * If the j-th and the (j+1)-th differ by 1,
+     * then we need to consider 2 candidates,
+     * one being T[i][j-1] + T[j+1][k], one being T[i][j] + T[j+2][k].
+     * Those T[m][n] with {m > n} are deemed as 0.
+     */
+
     std::sort(nums.begin(), nums.end());
-    int ret = 0;
-    for (auto i0 = nums.begin(), i1 = nums.begin(); i1 != nums.end(); i0 = i1) {
-        int partial_sums[2] = {0, 0};
-        partial_sums[(*i0) % 2] += *i0;
-        ++i1;
-        while (i1 != nums.end()) {
-            switch (*i1 - *i0) {
-            case 1:
-                i0 = i1;
-            case 0:
-                partial_sums[(*i0) % 2] += *i0;
-                ++i1;
-                continue;
-            }
-            break;
+    std::size_t distinct_terms = 0; // k; see comment above
+    for (auto i = nums.begin(), j = nums.begin(); j != nums.end(); i = j) {
+        ++distinct_terms;
+        while (j < nums.end() && *j == *i) {
+            ++j;
         }
-        ret += (partial_sums[0] > partial_sums[1]) ? partial_sums[0]
-                                                   : partial_sums[1];
     }
-    return ret;
+    TriangularMatrix T(distinct_terms);
+    std::vector<int> values(distinct_terms, 0);
+    std::size_t idx = 0;
+    for (auto itor_0 = nums.begin(), itor_1 = nums.begin();
+         itor_1 != nums.end(); itor_0 = itor_1, ++idx) {
+        int sum = 0;
+        while (itor_1 != nums.end() && *itor_1 == *itor_0) {
+            sum += *itor_1;
+            ++itor_1;
+        }
+        T.set(idx, idx, sum);
+        values.at(idx) = *itor_0;
+    }
+
+    for (idx = 1; idx < distinct_terms; ++idx) {
+        for (std::size_t offset = 0; offset < distinct_terms - idx; ++offset) {
+            // Calculate the subproblem T[row][col]
+            // of which values range from the (row)-th smallest to the (col)-th
+            const std::size_t row = offset;
+            const std::size_t col = offset + idx;
+            int sum = 0;
+
+            // For T[row][col], there are many ways to cut subproblem.
+            // For each subproblem, there are 4 cases to consider.
+            // 1. They are "disjoint", i.e. in sorted array, they differ by
+            // larger than scalar 1
+            // 2.(a) choose (m), discard (m+1)
+            // 2.(b) choose (m+1), discard (m)
+            // 2.(c) discard both (m) and (m+1)
+            for (std::size_t s = 0; s < idx; ++s) {
+                const std::pair<std::size_t, std::size_t> lhs =
+                    std::make_pair(row, row + s);
+                decltype(lhs) rhs = std::make_pair(row + s + 1, col);
+                int tmpSum = 0;
+                const int case1 =
+                    T.at(lhs.first, lhs.second) + T.at(rhs.first, rhs.second);
+                const int case2a = T.at(lhs.first, lhs.second) +
+                                   T.at(rhs.first + 1, rhs.second);
+                const int case2b = T.at(lhs.first, lhs.second - 1) +
+                                   T.at(rhs.first, rhs.second);
+                const int case2c = T.at(lhs.first, lhs.second - 1) +
+                                   T.at(rhs.first + 1, rhs.second);
+                tmpSum = (values.at(lhs.second) + 1 < values.at(rhs.first) &&
+                          case1 > tmpSum)
+                             ? case1
+                             : tmpSum;
+                tmpSum = (case2a > tmpSum) ? case2a : tmpSum;
+                tmpSum = (case2b > tmpSum) ? case2b : tmpSum;
+                tmpSum = (case2c > tmpSum) ? case2c : tmpSum;
+                if (tmpSum > sum) {
+                    sum = tmpSum;
+                }
+            }
+            T.set(row, col, sum);
+        }
+    }
+
+    return T.at(0, distinct_terms - 1);
 }
--- 740/soln.h
+++ 740/soln.h
@@ -1,6 +1,117 @@
+#include <utility>
 #include <vector>
 
 class Solution {
     public:
         int deleteAndEarn(std::vector<int> &nums);
+
+    private:
+        /* modified from https://stackoverflow.com/a/3279550
+         * Objective:
+         * Implement a triangular array using 1D array
+         * Conceptual indexing: (row,col)
+         * (For `mSize == 6`, hosting 21 entries)
+         *                    0,5
+         *                 1,5   0,4
+         *              2,5   1,4   0,3
+         *           3,5   2,4   1,3   0,2
+         *        4,5   3,4   2,3   1,2   0,1
+         *     5,5   4,4   3,3   2,2   1,1   0,0
+         * The underlying 1D array is s.t.
+         * (0,5), (0,4), (0,3), (0,2), (0,1), (0,0), (1,5), ...
+         * [0]    [1]    [2]    [3]    [4]    [5]    [6]    ...
+         */
+        class TriangularMatrix {
+            public:
+                // (default) constructor
+                TriangularMatrix(std::size_t size = 0)
+                    : mSize(size),
+                      privateRaw1DArr(
+                          mSize ? new int[((mSize % 2)
+                                               ? ((mSize + 1) / 2) * mSize
+                                               : (mSize / 2) * (mSize + 1))]()
+                                : nullptr),
+                      triangMatrix(mSize ? new int *[mSize]() : nullptr) {
+                    if (mSize) {
+                        triangMatrix[0] = privateRaw1DArr;
+                        for (std::size_t s = 1; s < mSize; ++s) {
+                            triangMatrix[s] =
+                                triangMatrix[s - 1] + ((mSize + 1) - s);
+                        }
+                    }
+                };
+
+                // copy-constructor
+                TriangularMatrix(const TriangularMatrix &other)
+                    : TriangularMatrix(other.mSize) {
+                    // Note that this is non-throwing, because of the data types
+                    // being used; more attention to detail with regards to
+                    // exceptions must be given in a more general case, however
+                    if (mSize) {
+                        std::copy(other.privateRaw1DArr,
+                                  other.privateRaw1DArr + mSize,
+                                  privateRaw1DArr);
+                    }
+                }
+
+                // copy-and-swap idiom
+                friend void swap(TriangularMatrix &first,
+                                 TriangularMatrix &second) {
+                    // enable ADL (not necessary in our case, but good practice)
+                    using std::swap;
+
+                    // by swapping the members of two objects,
+                    // the two objects are effectively swapped
+                    swap(first.mSize, second.mSize);
+                    swap(first.privateRaw1DArr, second.privateRaw1DArr);
+                    swap(first.triangMatrix, second.triangMatrix);
+                }
+
+                // copy-assignment operator
+                TriangularMatrix &operator=(TriangularMatrix other) {
+                    swap(*this, other);
+                    return *this;
+                }
+
+                // move constructor
+                // initialize via default constructor, C++11 only
+                TriangularMatrix(TriangularMatrix &&other) noexcept
+                    : TriangularMatrix() {
+                    swap(*this, other);
+                }
+
+                // dtor
+                ~TriangularMatrix() {
+                    delete[] privateRaw1DArr;
+                    delete[] triangMatrix;
+                }
+
+                int at(const std::size_t row, const std::size_t col) const {
+                    if (row <= col && col < mSize) {
+                        const auto coordinate = translateRowCol(row, col);
+                        return triangMatrix[coordinate.first]
+                                           [coordinate.second];
+                    } else {
+                        return 0;
+                    }
+                }
+
+                void set(const std::size_t row, const std::size_t col,
+                         const int v) {
+                    if (row <= col && col < mSize) {
+                        const auto coordinate = translateRowCol(row, col);
+                        triangMatrix[coordinate.first][coordinate.second] = v;
+                    }
+                }
+
+            private:
+                std::size_t mSize;
+                int *privateRaw1DArr;
+                int **triangMatrix;
+                inline std::pair<std::size_t, std::size_t>
+                translateRowCol(const std::size_t row,
+                                const std::size_t col) const {
+                    return std::make_pair(row, mSize - 1 - col);
+                }
+        };
 };
