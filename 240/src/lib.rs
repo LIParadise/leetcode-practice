358c55f297c0a33e82a922e71cf4b96d8353155a
LIParadise <a58524andy@gmail.com>
2022-10-19 14:41:59 +0800

    240 alternative soln: linear time

--- 240/src/lib.rs
+++ 240/src/lib.rs
@@ -9,25 +9,47 @@ impl Solution {
     /// Performs search in 2D matrix with following criteria:
     /// 1. Matrix, i.e. each row equal length
     /// 2. For each row/col, sorted in strictly ascending order
+    ///
+    /// Time complexity: O(width + height), linear time.
     pub fn search_matrix(matrix: Vec<Vec<i32>>, target: i32) -> bool {
+        // Claim:
+        // Since the rows/cols are sorted in ascending order, we could start
+        // from `matrix[0].last()` and do the following:
+        // 1. If hit, return true
+        // 2. Else if too large, move left
+        // 3. Else if too small, move down
+        // And we're guaranteed to find iff the target is in the matrix.
+        //
+        // Proof:
+        // Note we only discard values in step 2. and step 3.;
+        // in step 2. we discard those in the column *larger* than us,
+        // in step 3. we discard those in the row *smller* than us.
+        // Since we discard only numbers that are not equal to us, and
+        // obviously the specified operation runs till either a hit or discard
+        // whole matrix, we find the target iff the target is in the matrix.
         if matrix.len() == 0 || matrix[0].len() == 0 {
             return false;
         } else {
-            let mut search_end = matrix[0].len();
-            for row in matrix.iter() {
-                let candidate_idx = row
-                    .get(0..search_end)
-                    .unwrap()
-                    .partition_point(|num| num < &target);
-                if let Some(candidate) = row.get(candidate_idx) {
-                    if candidate == &target {
-                        return true;
+            let (mut r, mut c) = (0_usize, matrix[0].len() - 1);
+            loop {
+                match matrix[r][c].cmp(&target) {
+                    std::cmp::Ordering::Equal => return true,
+                    std::cmp::Ordering::Less => {
+                        if r == matrix.len() - 1 {
+                            return false;
+                        } else {
+                            r += 1;
+                        }
                     }
-                } else {
-                    search_end = candidate_idx;
+                    std::cmp::Ordering::Greater => {
+                        if c == 0 {
+                            return false;
+                        } else {
+                            c -= 1;
+                        }
+                    }
                 }
             }
-            return false;
         }
     }
 }
