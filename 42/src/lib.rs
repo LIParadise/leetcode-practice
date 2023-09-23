eec81d1291a684df2190ab4efb2fbf3ba983a2de
LIParadise <a58524andy@gmail.com>
2023-09-24 06:54:54 +0800

    O(n) time, O(1) space
    This brilliant insight comes from
    [here](https://leetcode.com/problems/trapping-rain-water/solutions/3401992/100-detailed-explaination-with-pictures-in-c-java-python-two-pointers/)

--- 42/src/lib.rs
+++ 42/src/lib.rs
@@ -4,60 +4,55 @@ pub struct Solution;
 
 impl Solution {
     pub fn trap(height: Vec<i32>) -> i32 {
-        // Idea: remember walls
+        // Idea:
+        // [Reference](https://leetcode.com/problems/trapping-rain-water/solutions/3401992/100-detailed-explaination-with-pictures-in-c-java-python-two-pointers)
         //
-        // A wall is s.t. where neighboring indices strictly decrease.
+        // Algorithm:
         //
-        // Now we have the interesting part: having a wall,
-        // where's the counterpart?
+        // Starting from both end, two pointers going inwards,
+        // each step record how much water it'll collect,
+        // while maintaining an auxiliary wall, i.e. the max height
+        // they've encountered.
         //
-        // Naive idea is that search for its partner till end, then again.
-        // This has O(n^2) worse case; consider something like the following
-        // [0, 100, 0, 99, 0, 98, 0, 97, 0, ..., 2, 0, 1, 0]
-        //
-        // Instead, we maintain a list of possible walls as follows:
-        // First, record our wall.
-        // For each number, if it's eligible for a wall, compare it with the
-        // smallest member in our list.
-        // While the new comer is no smaller, pop the smallest in the list,
-        // since the new comer would submerge the smaller one.
-        // If there's someone larger, stop, and append the new comer.
-        // If new comer is no smaller than our last i.e. largest,
-        // then the new comer IS the new wall,
-        // we record this pair for later rain calculation.
-        //
-        // If till end we haven't depleted our list of possible walls,
-        // this list records exactly where the walls are, and we're able to
-        // calculate the total water.
+        // While the two pointers going inwards:
+        // If both pointers were now below the two auxiliary walls,
+        // then we know an index that MUST be submerged,
+        // i.e. the guy at the side of which auxiliary wall is shorter,
+        // since it's between two walls.
+        // Actually we know EXACTLY how deep:
+        // it holds water till equal to its neighboring (shorter) auxiliary wall,
+        // since that's maximum wall height recorded from that side.
+        // Otherwise, either of the pointer is higher than previous wall,
+        // update corresponding auxiliary wall.
+        // This index would be dry after rain since there's no outer wall.
 
         let mut ret = 0;
-
-        let mut walls = Vec::new();
-        walls.push(0);
-        height.iter().enumerate().skip(1).for_each(|(i, &h)| {
-            let mut l_backup = walls.last().map(|tup| tup.clone());
-            while let Some(&l) = walls.last() {
-                l_backup = walls.last().map(|tup| tup.clone());
-                if h >= height[l] {
-                    walls.pop();
+        let mut l_ptr = height.iter().enumerate().peekable();
+        let mut r_ptr = height.iter().rev().enumerate().peekable();
+        let mut l_wall = 0;
+        let mut r_wall = 0;
+        while let (Some((l_idx, &l_h)), Some((r_idx, &r_h))) = (l_ptr.peek(), r_ptr.peek()) {
+            if l_idx + r_idx >= height.len() {
+                break;
+            } else {
+                if l_h > l_wall {
+                    l_wall = l_h;
+                    l_ptr.next();
+                } else if r_h > r_wall {
+                    r_wall = r_h;
+                    r_ptr.next();
+                } else if l_wall <= r_wall {
+                    ret += l_wall - l_h;
+                    l_ptr.next();
                 } else {
-                    break;
+                    ret += r_wall - r_h;
+                    r_ptr.next();
                 }
             }
-            if walls.is_empty() {
-                ret += Self::evaluate_walls(&height, l_backup.unwrap(), i);
-            }
-            walls.push(i);
-        });
-        walls.iter().zip(walls.iter().skip(1)).for_each(|(&l, &r)| {
-            ret += Self::evaluate_walls(&height, l, r);
-        });
+        }
+        r_ptr.next();
 
         ret
-    }
-    fn evaluate_walls(h_arr: &[i32], l: usize, r: usize) -> i32 {
-        let water_level = std::cmp::min(h_arr[l], h_arr[r]);
-        h_arr[l + 1..r].iter().map(|i| water_level - i).sum()
     }
 }
 
