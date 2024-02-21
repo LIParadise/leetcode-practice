3a7a15c233232853536a45fa4ce1f32cb69dd10d
LIParadise <a58524andy@gmail.com>
2024-02-21 13:01:14 +0800

    Refactored to Pascal's Triangle of nCr; TLE
    This way we mitigates the need for `u128` or alike which are to
    prevent overflow while calculating nCr using multiplication.
    
    Sadly this table requires insane amount of computation.

--- 1735/src/lib.rs
+++ 1735/src/lib.rs
@@ -3,35 +3,81 @@ pub struct Solution;
 impl Solution {
     pub fn ways_to_fill_array(queries: Vec<Vec<i32>>) -> Vec<i32> {
         /*
-         * 1. find prime factorization
-         * 2. supp. n primes, for m = 1 to n, merge primes (C(n,m) of them)
-         *    each merge contributes exactly the same # of conbinations
-         *    remember to pad 1
-         * 3. sum these
+         * 1. Prime factorization (with multiplicity)
+         * 2. Supp. one factor is p^m, array has n slots,
+         *    then it forms a nHm problem
+         *    where nHm denotes # of non-negative solutions
+         *    to the equation
+         *    x_1 + x_2 + ... + x_n = m
+         * 3. Mutliply them together, with possible overflow in mind.
          */
+
         let mut ret: Vec<i32> = Vec::new();
         let mut primes = Vec::new();
-        queries.iter().for_each(|q| {
-            // Eratosthenes, preparing for prime factorization
-            let mut num = q[1] as usize;
-            Self::extend_primes(&mut primes, num);
-            // Prime factorization
-            let multiplicity: Vec<usize> = primes.iter().fold(Vec::new(), |mut m, p| {
-                let mut cnt = 0;
-                while num % p == 0 {
-                    num /= p;
-                    cnt += 1;
+        match queries.iter().flatten().max() {
+            None => {}
+            Some(&ncr_max) => {
+                let ncr = Self::make_ncr(ncr_max as usize * 2 - 1);
+                queries.iter().for_each(|q| {
+                    // Eratosthenes, preparing for prime factorization
+                    let mut num = q[1] as usize;
+                    Self::extend_primes(&mut primes, num);
+                    // Prime factorization
+                    let multiplicity: Vec<usize> = primes.iter().fold(Vec::new(), |mut m, p| {
+                        let mut cnt = 0;
+                        while num % p == 0 {
+                            num /= p;
+                            cnt += 1;
+                        }
+                        if cnt != 0 {
+                            m.push(cnt);
+                        }
+                        m
+                    });
+                    ret.push(multiplicity.into_iter().fold(1, |acc, m| {
+                        Self::mod_p(
+                            acc as u64
+                                * *Self::access_ncr(q[0] as usize + m - 1, m, &ncr).unwrap() as u64,
+                        )
+                    }) as i32);
+                });
+            }
+        }
+        ret
+    }
+
+    fn make_ncr(s: usize) -> Vec<Vec<u32>> {
+        // Pascal's Triangle
+        // 1
+        // 1 1
+        // 1 2 1
+        // 1 3 3 1
+        // 1 4 6 4 1
+        // save only first half with prime modulo arithmetics
+        let mut ret = Vec::with_capacity(s + 1);
+        (0..=s).for_each(|i| ret.push(Vec::with_capacity(i / 2 + 1)));
+        ret[0].push(1);
+        (1..=s).for_each(|i| {
+            let (known, working) = ret.split_at_mut(i);
+            working.first_mut().unwrap().push(1);
+            working.first_mut().unwrap().extend((1..i / 2 + 1).map(|j| {
+                match known.last().unwrap().get(j) {
+                    Some(k) => k + known.last().unwrap()[j - 1],
+                    None => known.last().unwrap()[j - 1] * 2,
                 }
-                if cnt != 0 {
-                    m.push(cnt);
-                }
-                m
-            });
-            ret.push(multiplicity.into_iter().fold(1, |acc, m| {
-                Self::mod_p(Self::nhr(q[0] as usize, m) as u128 * acc as u128) as usize
-            }) as i32);
+            }));
+            working
+                .first_mut()
+                .unwrap()
+                .iter_mut()
+                .for_each(|i| *i = Self::mod_p(*i as u64));
         });
         ret
+    }
+
+    fn access_ncr(n: usize, r: usize, ncr: &Vec<Vec<u32>>) -> Option<&u32> {
+        let r = if r < n / 2 + 1 { r } else { n - r };
+        ncr.get(n).map(|v| v.get(r)).flatten()
     }
 
     fn extend_primes(known_primes: &mut Vec<usize>, tgt: usize) {
@@ -75,81 +121,19 @@ impl Solution {
                                     })
                             }
                         });
-                    tmp.into_iter()
-                        .enumerate()
-                        .filter_map(|(i, b)| b.then_some(i + known_max_p + 1))
-                        .for_each(|p| {
-                            known_primes.push(p);
-                        });
+                    known_primes.extend(
+                        tmp.into_iter()
+                            .enumerate()
+                            .filter_map(|(i, b)| b.then_some(i + known_max_p + 1)),
+                    );
                 }
             }
         }
     }
 
-    fn mod_p(i: u128) -> u128 {
-        static P: u128 = 1_000_000_007;
-        i % P
-    }
-
-    /// Simple nCr implementation
-    /// Assuming non-negative semantics
-    fn ncr(n: usize, r: usize) -> usize {
-        if r > n {
-            0
-        } else {
-            let r = r as u128;
-            let n = n as u128;
-            Self::mod_p((1..=r.min(n - r)).fold(1, |acc, val| acc * (n - val + 1) / val)) as usize
-        }
-    }
-
-    /// # of non-negative integer solutions to the following?
-    /// x_1 + x_2 + ... + x_n = r
-    /// answer is nHr or nhr(n,r)
-    fn nhr(n: usize, r: usize) -> usize {
-        Self::ncr(n + r - 1, r)
-    }
-
-    fn extend_num_of_prime_factors(arr: &mut Vec<i32>, tgt: usize) {
-        let old_len = arr.len();
-        if tgt >= arr.len() {
-            let mut new = Vec::with_capacity(tgt - old_len + 1);
-            (0..tgt - old_len + 1).for_each(|_| new.push(1));
-            arr.iter()
-                .enumerate()
-                .filter(|(_, &cnt)| cnt == 1)
-                .for_each(|(p, _)| {
-                    (std::cmp::max((1 + (old_len - 1) / p) * p, p * p)..=tgt)
-                        .step_by(p)
-                        .for_each(|i| {
-                            new[i - old_len] = 1 + if i / p < old_len {
-                                arr[i / p]
-                            } else {
-                                new[i / p - old_len]
-                            };
-                        });
-                });
-            arr.extend(new.into_iter());
-            let mut search_after_here = old_len - 1;
-            while let Some(p) = arr
-                .iter()
-                .enumerate()
-                .skip(search_after_here + 1)
-                .find(|(_, &cnt)| cnt == 1)
-                .map(|(p, _)| p)
-            {
-                search_after_here = p;
-                if (arr.len() - 1) / p < p {
-                    break;
-                }
-                (p * p..arr.len()).step_by(p).for_each(|i| {
-                    let (left, right) = arr.split_at_mut(i);
-                    if let Some(1) = right.first() {
-                        *right.first_mut().unwrap() = left[i / p] + 1;
-                    }
-                });
-            }
-        }
+    fn mod_p(i: u64) -> u32 {
+        static P: u64 = 1_000_000_007;
+        (i % P) as u32
     }
 }
 
@@ -164,14 +148,21 @@ mod tests {
             vec![4, 1, 50734910]
         );
         assert_eq!(
+            Solution::ways_to_fill_array(vec![vec![14, 7], vec![20, 3], vec![13, 9], vec![4, 9]]),
+            vec![]
+        );
+        assert_eq!(
             Solution::ways_to_fill_array(vec![
                 vec![1, 1],
                 vec![2, 2],
                 vec![3, 3],
                 vec![4, 4],
-                vec![5, 5]
+                vec![5, 5],
+                vec![10, 1],
+                vec![10, 5],
+                vec![1, 60]
             ]),
-            vec![1, 2, 3, 10, 5]
+            vec![1, 2, 3, 10, 5, 1, 10, 1]
         );
         assert_eq!(
             Solution::ways_to_fill_array(vec![vec![3360, 1536], vec![6850, 6227]]),
@@ -180,38 +171,29 @@ mod tests {
     }
 
     #[test]
-    fn test_extend_num_of_prime_factors() {
-        let mut arr = vec![0, 0, 1, 1];
-        Solution::extend_num_of_prime_factors(&mut arr, 21);
-        assert_eq!(
-            vec![0, 0, 1, 1, 2, 1, 2, 1, 3, 2, 2, 1, 3, 1, 2, 2, 4, 1, 3, 1, 3, 2],
-            arr
-        );
-        /*
-         * [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21]
-         *        2  3  2  5  2  7  2  3   2  11   2  13   2   3   2  17   2  19   2   3
-         *              2     3     2  3   5       2       7   5   2       3       2   7
-         *                          2              3               2       3       5
-         *                                                         2
-         * [0, 0, 1, 1, 2, 1, 2, 1, 3, 2,  2,  1,  3,  1,  2,  2,  4,  1,  3,  1,  3,  2]
-         */
-        let mut arr = vec![0, 0];
-        Solution::extend_num_of_prime_factors(&mut arr, 78);
-        /*
-         * [OEIS](https://oeis.org/wiki/Prime_factors)
-         * [0, 0, 1, 1, 2, 1, 2, 1, 3, 2, 2, 1, 3, 1, 2, 2, 4, 1, 3, 1, 3, 2, 2, 1, 4, 2, 2, 3, 3, 1,
-         *  3, 1, 5, 2, 2, 2, 4, 1, 2, 2, 4, 1, 3, 1, 3, 3, 2, 1, 5, 2, 3, 2, 3, 1, 4, 2, 4, 2, 2,
-         *  1, 4, 1, 2, 3, 6, 2, 3, 1, 3, 2, 3, 1, 5, 1, 2, 3, 3, 2, 3]
-         * Notice here I start with 0 (TBH kinda ill-formed) instead of 1.
-         * */
-    }
-
-    #[test]
     fn test_comb() {
-        assert_eq!(Solution::ncr(5, 3), Solution::ncr(5, 2));
-        assert_eq!(Solution::ncr(10, 4), 210);
-        assert_eq!(Solution::nhr(3, 2), 6);
-        assert_eq!(Solution::nhr(5, 1), 5);
+        let ncr = Solution::make_ncr(5);
+        assert_eq!(Solution::access_ncr(0, 0, &ncr), Some(&1));
+        assert_eq!(Solution::access_ncr(1, 0, &ncr), Some(&1));
+        assert_eq!(Solution::access_ncr(1, 1, &ncr), Some(&1));
+        assert_eq!(Solution::access_ncr(2, 0, &ncr), Some(&1));
+        assert_eq!(Solution::access_ncr(2, 1, &ncr), Some(&2));
+        assert_eq!(Solution::access_ncr(2, 2, &ncr), Some(&1));
+        assert_eq!(Solution::access_ncr(3, 0, &ncr), Some(&1));
+        assert_eq!(Solution::access_ncr(3, 1, &ncr), Some(&3));
+        assert_eq!(Solution::access_ncr(3, 2, &ncr), Some(&3));
+        assert_eq!(Solution::access_ncr(3, 3, &ncr), Some(&1));
+        assert_eq!(Solution::access_ncr(4, 0, &ncr), Some(&1));
+        assert_eq!(Solution::access_ncr(4, 1, &ncr), Some(&4));
+        assert_eq!(Solution::access_ncr(4, 2, &ncr), Some(&6));
+        assert_eq!(Solution::access_ncr(4, 3, &ncr), Some(&4));
+        assert_eq!(Solution::access_ncr(4, 4, &ncr), Some(&1));
+        assert_eq!(Solution::access_ncr(5, 0, &ncr), Some(&1));
+        assert_eq!(Solution::access_ncr(5, 1, &ncr), Some(&5));
+        assert_eq!(Solution::access_ncr(5, 2, &ncr), Some(&10));
+        assert_eq!(Solution::access_ncr(5, 3, &ncr), Some(&10));
+        assert_eq!(Solution::access_ncr(5, 4, &ncr), Some(&5));
+        assert_eq!(Solution::access_ncr(5, 5, &ncr), Some(&1));
     }
 
     #[test]
