c49c4c584e678f547bd2e5bcc4d85b55d71284b5
LIParadise <a58524andy@gmail.com>
2024-03-23 03:03:25 +0800

    [cargo clippy] refactor a bit

--- 67/src/lib.rs
+++ 67/src/lib.rs
@@ -9,38 +9,40 @@ impl Solution {
         };
         let mut ret = Vec::new();
         let mut carry = 0;
-        a.iter()
-            .rev()
-            .zip(b.iter().rev().chain(std::iter::repeat(&('0' as u8))))
-            .for_each(|(&a, &b)| {
-                let a = a - '0' as u8;
-                let b = b - '0' as u8;
-                match carry + a + b {
-                    3 => {
-                        carry = 1;
-                        ret.push(true)
+        ret.extend(
+            a.iter()
+                .rev()
+                .zip(b.iter().rev().chain(std::iter::repeat(&b'0')))
+                .map(|(&a, &b)| {
+                    let a = a - b'0';
+                    let b = b - b'0';
+                    match carry + a + b {
+                        3 => {
+                            carry = 1;
+                            true
+                        }
+                        2 => {
+                            carry = 1;
+                            false
+                        }
+                        1 => {
+                            carry = 0;
+                            true
+                        }
+                        0 => {
+                            carry = 0;
+                            false
+                        }
+                        _ => panic!("Addition fails: bit overflow"),
                     }
-                    2 => {
-                        carry = 1;
-                        ret.push(false)
-                    }
-                    1 => {
-                        carry = 0;
-                        ret.push(true)
-                    }
-                    0 => {
-                        carry = 0;
-                        ret.push(false)
-                    }
-                    _ => panic!("Addition fails: bit overflow"),
-                }
-            });
+                }),
+        );
         if carry == 1 {
             ret.push(true);
         }
         ret.into_iter()
             .rev()
-            .map(|b| b.then(|| '1').unwrap_or('0'))
+            .map(|b| if b { '1' } else { '0' })
             .collect()
     }
 }
