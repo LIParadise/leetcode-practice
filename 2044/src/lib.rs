4e8c4a94120516b9c34053ea2f28df1451176307
LIParadise <a58524andy@gmail.com>
2024-04-11 16:29:13 +0800

    Avoid non-necessary recursions
    Time complexity should be O(2^n)
    Space complexity should be O(n)

--- 2044/src/lib.rs
+++ 2044/src/lib.rs
@@ -5,31 +5,41 @@ impl Solution {
     pub fn count_max_or_subsets(nums: Vec<i32>) -> i32 {
         let target = nums.iter().fold(0, |target, n| target | n);
         let mut ret = 0;
-        Self::powerset_bitwise_or_count_worker(&target, &vec![], 0, &nums, &mut ret);
+        Self::powerset_bitwise_or_count_worker(&target, 0, 0, &nums, &mut ret);
         ret.try_into().unwrap()
     }
     fn powerset_bitwise_or_count_worker(
         target: &i32,
-        part: &[i32],
+        partial_or_sum: i32,
         index: usize,
         set: &[i32],
         powerset_size: &mut usize,
     ) {
         match set.get(index) {
             Some(&i) => {
-                Self::powerset_bitwise_or_count_worker(target, part, index + 1, set, powerset_size);
-                let mut part = part.to_owned();
-                part.push(i);
-                Self::powerset_bitwise_or_count_worker(
-                    target,
-                    &part,
-                    index + 1,
-                    set,
-                    powerset_size,
-                );
+                if partial_or_sum == *target {
+                    // OR operator only attributes to more 1s
+                    // early stop to prevent non-necessary recursion
+                    *powerset_size += 2_usize.pow(set.len() as u32 - index as u32)
+                } else {
+                    Self::powerset_bitwise_or_count_worker(
+                        target,
+                        partial_or_sum,
+                        index + 1,
+                        set,
+                        powerset_size,
+                    );
+                    Self::powerset_bitwise_or_count_worker(
+                        target,
+                        partial_or_sum | i,
+                        index + 1,
+                        set,
+                        powerset_size,
+                    );
+                }
             }
             None => {
-                if part.iter().fold(0, |or_result, i| i | or_result) == *target {
+                if partial_or_sum == *target {
                     *powerset_size += 1;
                 }
             }
