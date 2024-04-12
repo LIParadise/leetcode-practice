7541888029feb82b4a39fcd331b0ba4f6b87ac38
LIParadise <a58524andy@gmail.com>
2024-04-12 16:36:18 +0800

    [permutation] An fast and interesting iterative approach
    Idea from [issac3](https://leetcode.com/issac3)
    https://leetcode.com/problems/permutations/solutions/18239/a-general-approach-to-backtracking-questions-in-java-subsets-permutations-combination-sum-palindrome-partioning

--- 46/src/lib.rs
+++ 46/src/lib.rs
@@ -8,42 +8,44 @@ impl Solution {
     /// 1 <= nums.length <= 6
     /// -10 <= nums[i] <= 10
     pub fn permute(nums: Vec<i32>) -> Vec<Vec<i32>> {
-        /*
-         * Consider doing permutations for "abcde"
-         *
-         * 1 => a
-         * 2 => ab, ba
-         * 3 => consider where to put 'c';
-         *      3 locations for "ab"
-         *      3 locations for "ba"
-         *      They are all different, and they together are all.
-         * 4 => consider where to put 'd', 4*6 = 24
-         * So on and so forth.
-         *
-         * I.e. build the permutations by parts, consider characters one by one
-         * For every new character, consider all available indices it could be, insert it to that
-         * index
-         * Do such to all the previously built permutation of substrings.
-         */
-        let mut ret: Vec<Vec<i32>> = Vec::new();
-        let mut workspace = Vec::new();
-        for n in nums {
-            if ret.is_empty() {
-                workspace.push(vec![n]);
-            } else {
-                workspace.clear();
-                for permutation_of_substring in &ret {
-                    for idx in 0..=permutation_of_substring.len() {
-                        let mut copied = permutation_of_substring.clone();
-                        copied.insert(idx, n);
-                        workspace.push(copied);
-                    }
-                }
-            }
-            ret.clear();
-            ret.extend(workspace.iter().cloned());
-        }
+        let mut permutation = Vec::with_capacity(nums.len());
+        let mut ret = Vec::with_capacity((1..=nums.len()).product());
+        Self::permute_worker(&mut permutation, &nums, &mut ret);
         ret
+    }
+    // Idea: for every element, try put them at every index
+    //
+    // Consider [a, b, c]
+    //
+    // Upon first call, recursive call self, s.t.
+    // call depth          : 1    2       3
+    // permutation content : [a]  [a, b]  [a, b, c]
+    // and we push [a, b, c] to the collection
+    //
+    // Next, call depth 3 pops and ends the for-loop, back to call depth 2
+    // call depth, while having [a, b], further pops, resulting in [a]
+    // the for-loop of call depth 2 hasn't ended yet, this time considering c,
+    // yielding [a, c], then dive to call depth 3, this time giving [a, c, b]. 
+    //
+    // So on and so forth, at each call depth d,
+    // the function chooses one element to be at index (d-1) to make a new permutation,
+    // and it does so for each element, s.t. all permutations are visited.
+    //
+    // Pros: consumes little space (O(n)), probably cache-friendly
+    // Cons: abyssmal time complexity (W(n!*n^2)), due to linear search in array
+    // Possible mitigation: add hash, double the space while time complexity becomes O(n!)
+    fn permute_worker(permutation: &mut Vec<i32>, nums: &[i32], permutations: &mut Vec<Vec<i32>>) {
+        if permutation.len() == nums.len() {
+            permutations.push(permutation.clone());
+        } else {
+            nums.iter().for_each(|n| {
+                if !permutation.contains(n) {
+                    permutation.push(*n);
+                    Self::permute_worker(permutation, nums, permutations);
+                    permutation.pop();
+                }
+            });
+        }
     }
 }
 
