832f3f9c171c59014786b8b44f1e90631d9ad81e
LIParadise <a58524andy@gmail.com>
2024-04-17 13:59:42 +0800

    [powerset] [backtrack] issac3@LC's incredible formulation
    https://leetcode.com/problems/permutations/solutions/18239/a-general-approach-to-backtracking-questions-in-java-subsets-permutations-combination-sum-palindrome-partioning

--- 90/src/lib.rs
+++ 90/src/lib.rs
@@ -8,45 +8,54 @@ impl Solution {
     pub fn subsets_with_dup(mut nums: Vec<i32>) -> Vec<Vec<i32>> {
         nums.sort_unstable();
         let mut ret = Vec::new();
-        Self::subsets_with_dup_worker(&nums, &mut ret, &mut Vec::new(), None, 0);
+        Self::subsets_with_dup_worker(&nums, &mut ret, &mut Vec::new());
         ret
     }
 
-    // DFS backtrack binary tree
-    // For each element, there's a boolean question: either to take or not to take,
-    // which is why the tree is binary; recursive call this function to DFS backtrack.
+    // issac3's genius algorithm, combining ideas of dynamic programming, backtrack, DFS, and
+    // iterations.
+    // https://leetcode.com/problems/permutations/solutions/18239/a-general-approach-to-backtracking-questions-in-java-subsets-permutations-combination-sum-palindrome-partioning
     //
-    // To avert duplicates, first sort the array, then upon decision of whether to put element
-    // in subset, if the answer is no, tell the immediate deeper recursion to also skip that
-    // member.
-    //
-    // This works since the array is sorted, s.t. equal elements are neighbors, and that it forces
-    // all the subsets in the powerset to have the following property:
-    // if one were to index each element in the sorted input array, then among all the duplicate
-    // subsets, the algorithm picks only the one in which sum of index is smallest.
-    //
-    // In layman's terms, among all duplicate subsets, the algorithm chooses the one appears
-    // "earliest" in the input array.
-    fn subsets_with_dup_worker(
-        nums: &[i32],
-        powerset: &mut Vec<Vec<i32>>,
-        subset: &mut Vec<i32>,
-        ignore: Option<i32>,
-        start_idx: usize,
-    ) {
-        if start_idx == nums.len() {
-            powerset.push(subset.to_owned());
-        } else if let Some(&n) = nums.get(start_idx) {
-            Self::subsets_with_dup_worker(nums, powerset, subset, Some(n), start_idx + 1);
-            if let Some(i) = ignore {
-                if i == n {
-                    return;
-                }
+    // For each element, there's the boolean question: either to take or not to take,
+    // which is why the DFS tree is binary: recursive call this function to DFS backtrack.
+    fn subsets_with_dup_worker(nums: &[i32], powerset: &mut Vec<Vec<i32>>, subset: &mut Vec<i32>) {
+        // We're thinking of subsets, hence we could do early-stop here:
+        // The subset here is subset of previously considered element, meaning it's subset of the
+        // whole input, which in turn means we could early-stop and record it here.
+        // This way saves us some calculations, e.g. we don't have to scan whole array only to
+        // conclude that the empty set is part of the powerset.
+        powerset.push(subset.to_owned());
+        if nums.is_empty() {
+            return;
+        }
+        nums.iter().enumerate().for_each(|(i, &n)| {
+            // At recursion depth d, choose the element to be in some subset in which the
+            // element resides at index (d-1).
+            // In principle every element is possible, hence iterate through them.
+            if i > 0 && nums[i] == nums[i - 1] {
+                // To avert duplicates, amongst the duplicates of some subset, choose the one in
+                // which every element appeared earliest in the input.
+                // E.g. set be [a, b, b], imagine indexing s.t. it's secretly [a0, b1, b2],
+                // consider subset [a, b]:
+                // choose [a0, b1] rather than [a0, b2].
+                //
+                // How?
+                // First, SORT the array s.t. equal elements are neighbors.
+                // Second, suppose there are multiple instances of same element e,
+                // if we skipped e_i, then we shall also skip all e_j where j > i,
+                // this way we eliminate the possibility where we choose some subset in which e_i
+                // is absent whereas e_j is present.
+                //
+                // If the condition holds, we're excluding the possibility that the repeating
+                // element were to be at the current index (recursion depth d i.e. index (d-1))
+                // By aforementioned observation namely if e_i were skipped then all e_j where
+                // j > i shall also be skipped, we do as required.
+                return;
             }
             subset.push(n);
-            Self::subsets_with_dup_worker(nums, powerset, subset, None, start_idx + 1);
+            Self::subsets_with_dup_worker(&nums[i + 1..], powerset, subset);
             subset.pop();
-        }
+        });
     }
 }
 
