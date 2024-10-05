87f087dabb46d548859f63540bb67d1de9462c45
LIParadise <a58524andy@gmail.com>
2024-10-05 13:45:33 +0800

    array to speed things up instead of map data structure

--- 312_rust/src/lib.rs
+++ 312_rust/src/lib.rs
@@ -1,12 +1,10 @@
 pub struct Solution;
-pub(crate) use std::collections::HashMap;
 
 impl Solution {
     pub fn max_coins(mut nums: Vec<i32>) -> i32 {
         if nums.is_empty() {
             0
         } else {
-            let mut dp = HashMap::<(usize, usize), i32>::new();
             let nums = {
                 let mut v = Vec::with_capacity(nums.len() + 2);
                 v.push(1);
@@ -14,41 +12,78 @@ impl Solution {
                 v.push(1);
                 v
             };
-            let ret = Self::max_coins_worker(&nums, (0, nums.len() - 1), &mut dp);
+
+            // To reduce memory footprint, access needs some index hack:
+            //
+            // Given subproblem with left sentinel index and right sentinel index,
+            // the access to the DP memoization is [left][right - left - 2]
+            //
+            // E.g. consider `[3, 1, 4, 1, 5]`,
+            // after the expansion with virtual sentinels we get `[1, 3, 4, 1, 5, 1]`,
+            // and the problem is asking left sentinel idx 0 and right sentinel idx 5,
+            // and it could be found at `[0][5 - 0 - 2]` i.e. `[0][3]`
+            // similarly for subproblem `[3, 4, 1, 5]` it's `[1][4 - 1 - 2]` i.e. `[1][1]`
+            let mut ddp: Vec<Vec<Option<i32>>> =
+                Vec::from_iter((0..nums.len() - 2).map(|left_sentinel| {
+                    Vec::from_iter((left_sentinel + 2..nums.len()).map(|_right_sentinel| None))
+                }));
+
+            let ret = Self::max_coins_worker(&nums, (0, nums.len() - 1), &mut ddp);
             ret
         }
     }
+
+    /// Given list of balloons and two ordered indices which cannot be popped,
+    /// consider the balloons in between:
+    /// what's the best strategy popping the in-between balloons?
+    ///
+    /// The algorithm starts by choosing one by one the *last* balloon to be popped, making the
+    /// problem recursive and applicable for dynamic programming.
     fn max_coins_worker(
         nums: &[i32],
-        sentinels @ (left_sentinel, right_sentinel): (usize, usize),
-        dp: &mut HashMap<(usize, usize), i32>,
+        (left_sentinel, right_sentinel): (usize, usize),
+        ddp: &mut [Vec<Option<i32>>],
     ) -> i32 {
-        // both `first` and `last` are NOT to be popped
+        // both `left_sentinel` and `right_sentinel` are NOT to be popped
         //
         // choose one index to be the LAST one popped;
         // such config is easy to calculate the points
         //
         // iterate over all possible indices, then find max.
-
         if left_sentinel + 1 >= right_sentinel {
             0
         } else {
-            if dp.get(&sentinels).is_none() {
-                // `last` larger, so minus 1 is OK
-                let ret = (left_sentinel..right_sentinel)
-                    .skip(1)
-                    .map(|mid_sentinel| {
-                        let left = Self::max_coins_worker(nums, (left_sentinel, mid_sentinel), dp);
-                        let right =
-                            Self::max_coins_worker(nums, (mid_sentinel, right_sentinel), dp);
-                        left + right
-                            + nums[mid_sentinel] * nums[left_sentinel] * nums[right_sentinel]
-                    })
-                    .max()
-                    .unwrap();
-                dp.insert(sentinels, ret);
-            }
-            dp.get(&sentinels).cloned().unwrap()
+            let subproblem_ans = match ddp.get_mut(left_sentinel).and_then(|v|
+                    // memory footprint indexing hack;
+                    // see the calling function for why this is the case
+                    v.get_mut(right_sentinel - left_sentinel - 2))
+            {
+                Some(None) => {
+                    // `last` larger, so minus 1 is OK
+                    (left_sentinel..right_sentinel)
+                        .skip(1)
+                        .map(|mid_sentinel| {
+                            let left =
+                                Self::max_coins_worker(nums, (left_sentinel, mid_sentinel), ddp);
+                            let right =
+                                Self::max_coins_worker(nums, (mid_sentinel, right_sentinel), ddp);
+                            left + right
+                                + nums[mid_sentinel] * nums[left_sentinel] * nums[right_sentinel]
+                        })
+                        .max()
+                        .unwrap()
+                }
+                Some(Some(dp_entry)) => *dp_entry,
+                None => unreachable!(),
+            };
+
+            *ddp.get_mut(left_sentinel)
+                .and_then(|v|
+                    // memory footprint indexing hack;
+                    // see the calling function for why this is the case
+                    v.get_mut(right_sentinel - left_sentinel - 2))
+                .unwrap() = Some(subproblem_ans);
+            subproblem_ans
         }
     }
 }
